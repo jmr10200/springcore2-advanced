52eb1f13cf5b24cc5d6d183d948e59ecf30e80ef
jmr <jmr10200@gmail.com>
2022-09-23 15:16:26 +0900

    ThreadLocal - 적용 및 주의사항

--- src/test/java/hello/advanced/trace/logtrace/ThreadLocalLogTraceTest.java
+++ src/test/java/hello/advanced/trace/logtrace/ThreadLocalLogTraceTest.java
@@ -27,3 +27,21 @@ class ThreadLocalLogTraceTest {
         trace.exception(status1, new IllegalStateException());
     }
 }
+/* ThreadLocal 주의사항 */
+// 쓰레드 로컬을 사용할 때 주의해야할 것이 있다.
+// 사용 후 값을 remove() 로 제거하지 않으면, WAS(톰캣) 처럼 쓰레드 풀을 사용하는 경우 심각한 문제가 발생할 수 있다.
+
+// 예를들어
+// userA 가 HTTP 요청을 한다.
+// WAS 는 쓰레드풀에서 Thread-1 을 할당한다.
+// Thread-1 로 데이터를 보관한다.
+// userA 는 Thread-1 의 값을 제거하지 않고, 쓰레드풀에 Thread-1 을 반납한다
+// (쓰레드 생성비용은 비싸기 때문에 쓰레드 풀에 보관해놓고 재사용한다)
+// userX 가 HTTP 요청을 한다.
+// WAS 는 쓰레드풀에서 Thread-1 을 할당한다.
+// userX 의 데이터를 조회한다.
+// Thread-1 로 조회된 데이터가 userA 의 데이터가 된다.
+
+// 즉, userX 가 userA 의 정보를 조회하게되는 심각한 문제가 발생한다.
+// 이러한 문제 때문에
+// 쓰레드 로컬을 사용하고 난 후에는 꼭 ThreadLocal.remove() 를 호출해 값을 제거해야한다.
