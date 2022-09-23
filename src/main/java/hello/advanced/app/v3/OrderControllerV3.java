cc746cf84a47fb8080f7a7f0de3bab91386456f0
jmr <jmr10200@gmail.com>
2022-09-23 14:15:07 +0900

    필드 동기화 - 적용

---
+++ src/main/java/hello/advanced/app/v3/OrderControllerV3.java
@@ -0,0 +1,32 @@
+package hello.advanced.app.v3;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV3 {
+
+    private final OrderServiceV3 orderService;
+    private final LogTrace trace;
+
+    @GetMapping("/v3/request") // HTTP 파라미터로 itemId
+    public String request(String itemId) {
+        TraceStatus status = null;
+
+        try {
+            status = trace.begin("OrderController.request()"); // 로그추적기 시작
+            orderService.orderItem(itemId);
+            trace.end(status); // 로그추적기 끝
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 다시 던져줘야 함
+        }
+        // 문제 : begin(), end() 뿐만아니라 exception() 도 처리해줘야한다. 더불어 try~catch 도 추가된다.
+    }
+}
